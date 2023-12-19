d1a31a97b6fdcd502409192fb503bef2eb77fa69
Alejo Castañeda <fcastaneda@cidenet.com.co>
2023-12-18 20:56:42 -0500

    Setting up proyect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+
 
 @Component({
-  selector: 'app-root',
+  imports: [
+    RouterOutlet
+  ],
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'AppHotel';
+  title = 'AppHotel'
+
+
+  private primengConfig = inject(PrimeNGConfig)
+
+
+  ngOnInit() {
+    this.primengConfig.ripple = true
+  }
 }
