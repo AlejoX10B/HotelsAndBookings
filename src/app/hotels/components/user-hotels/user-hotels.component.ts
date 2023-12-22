89b2c8d97e289aa02c102f2f0c55a7c080326e69
Alejo Castañeda <fcastaneda@cidenet.com.co>
2023-12-21 19:20:18 -0500

    Adding user hotels list

---
+++ src/app/hotels/components/user-hotels/user-hotels.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-hotels',
+  templateUrl: './user-hotels.component.html',
+  styleUrl: './user-hotels.component.scss'
+})
+export class UserHotelsComponent {
+
+}
